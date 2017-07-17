fc430f10aff68c89b985b94fd267f5b48fbad867
Nils3311 <hofnil@web.de>
2017-07-17 16:50:07 +0200

    Überarbeitet

--- Laos Optimization.py
+++ Laos Optimization.py
@@ -14,48 +14,53 @@ import numpy as np
 
 #Time
 T = [1,2,3,4,5]
+model = ConcreteModel()
+model.T = Set(initialize = T)
 
-''' ------------Variables------------ '''
+#==============================================================================
+# ------------Variables------------
+#==============================================================================
 #Wind
 CostWind = 1000000 #€/MW
 LifetimeWind = 20 #a
 FactorWind = [0.5, 0.3, 0.1, 0.6, 0.2] #Wind Factor @ hour X
 
 #PV
-CostPv = 400000 #€/MWp
+CostPv = 1000000 #€/MWp
 LifetimePv= 30 #a
-FactorPv = [0.2, 0.3, 0.8, 0.4, 0.02] #Radiation Factor @ hour X
+FactorPv = [0.5, 0.3, 0.8, 0.4, 0.0] #Radiation Factor @ hour X
 
 #Water
-Storage = 10000000 #m^3 in Storage, hourly correction
+StorageSize = 20000
+StorageVariable = [StorageSize for i in range(0,len(T)+1)] #m^3 in Storage, hourly correction
 Pwater = 0.001 #MWh/m^3
 TurbineLimit = 480 #MW
 InflowRain = 40 #m^3 @ hour X
 InflowRiver = 200 #m^3 @ hour X
 
 #Demand
-DemandEnergy = [500, 400, 300, 450, 310] #MWh @ hour X
+DemandEnergy = [400, 500, 300, 450, 310] #MWh @ hour X
 DemandWater = [200, 300, 400, 301, 100] #m^3/h @ hour X
 
-''' ------------COST------------ '''
+#==============================================================================
+# ------------COST------------
+#==============================================================================
 #Costcalculation
 Cwind = CostWind #€/MW Wind, 1 year only
 Cpv = CostPv #€/MW PV, 1 year only
-Cwater = 10000 #€/MWh
+Cwater = 0 #€/MWh
 
-''' ------------MODEL CONSTRUCTION------------ '''
-model = ConcreteModel()
-model.T = Set(initialize = T)
-
+#==============================================================================
+# ------------MODEL CONSTRUCTION------------
+#==============================================================================
 #Parameter
-#model.DemandWater = Set(initialize=DemandWater, ordered=True) #Water demand @ hour x
+model.DemandWater = Set(initialize=DemandWater, ordered=True) #Water demand @ hour x
 model.DemandEnergy = Set(initialize=DemandEnergy, ordered=True) #Energy demand @ hour X
 model.FactorWind = Set(initialize=FactorWind, ordered = True)
 model.FactorPv = Set(initialize=FactorPv, ordered = True)
 model.Cwind = Param(initialize=Cwind) #Possible MWh by Wind @ hour X
 model.Cpv = Param(initialize=Cpv) #Possible MWh by PV @ hour X
 model.Cwater = Param(initialize=Cwater)
-#model.Storage = Param(initialize=Storage, mutable = True)
 model.TurbineLimit = Param(initialize=TurbineLimit)
 model.Pwater = Param(initialize=Pwater)
 
@@ -64,7 +69,9 @@ model.Pwind = Var(domain=NonNegativeReals) #installed MW Wind
 model.Ppv = Var(domain=NonNegativeReals) #installed MW Wind
 model.UsageWater = Var(model.T, domain=NonNegativeReals) #used M^3 Water @ hour X
 
-''' ----------Constraint & Objective ------------ '''
+#==============================================================================
+# ----------Constraint & Objective & Solver------------
+#==============================================================================
 #------Objective Function-------
 #€/MW * MW + €/MW + m^3 * MWh/m^3 * €/MWh
 def obj_rule(model):
@@ -81,34 +88,40 @@ model.EnergyDemand = Constraint(model.T, rule=DemandEnergy_rule)
 
 #Limitation through Water Turbine
 def MaxWaterPower_rule(model, i):
-    return(model.UsageWater[i] * model.Pwater  <= model.TurbineLimit)
+    return(model.UsageWater[i] * model.Pwater  <= 480)
 model.MaxWaterPower = Constraint(model.T, rule=MaxWaterPower_rule)
 
-#Solver
+#--------STORAGE--------
+def WaterUsage_rule (model,i):
+    StorageVariable[i] = StorageVariable[i-1] - model.DemandWater[i] - model.UsageWater[i].value + InflowRain + InflowRiver
+    return(StorageVariable[i] >= model.Demand[i])
+model.WaterDemand = Constraint(model.T, rule=WaterUsage_rule)
+
+#------SOLVER---------
 opt = SolverFactory('glpk')
 model.write('optimization_problem.lp',
          io_options={'symbolic_solver_labels': True})
-
-''' ------------OUTPUT------------ '''
-#Ausgabe
-#model.pprint()
-
 results = opt.solve(model, tee=True)
 
-#print(results)
+#==============================================================================
+# ------------OUTPUT------------
+#==============================================================================
 print("")
 print("Wind installiert:", round(model.Pwind.value, 2), "MWh")
 print("PV installiert:", round(model.Ppv.value, 2), "MWh")
 print("Gesamt benötigtes Wasser:", round(sum(model.UsageWater[i].value for i in model.T),2), "m^3")
 print("")
 for i in model.T:
-    print("Stunde", i)
-    print("Demand:",model.DemandEnergy[i], "MWh")
+    print("Wasser im Speicher:", StorageVariable[i])
+    print("Nach Stunde", i)
+    print("Energy Demand:",model.DemandEnergy[i], "MWh")
+    print("Water Demand:", model.DemandWater[i], "m^3")
     print("Zusammensetzung:", 
           round(model.FactorPv[i]*model.Ppv.value,2), "MWh PV |", 
           round(model.FactorWind[i]*model.Pwind.value,2), "MWh Wind |", 
           round(model.UsageWater[i].value*Pwater,2), "MWh Wasser")
-    print(round(model.UsageWater[i].value,2), "m^3 gebrauchtes Wasser")
+    print(round(model.UsageWater[i].value,2), "m^3 Wasser für Energieerzeugung")
+    print("")
 print("")
 print("Wind:", model.Cwind * model.Pwind.value, "€ |", Cwind, "€ pro MWh" )
 print("PV:", model.Cpv * model.Ppv.value, "€ |", Cpv, "€ pro MWh" )
