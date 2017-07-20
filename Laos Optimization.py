46d895f35b43d4c974ef77b72c26af4ff1196ca8
Rolix193 <felix_roling@web.de>
2017-07-20 10:47:00 +0200

    Costs
    LOL

--- Laos Optimization.py
+++ Laos Optimization.py
@@ -6,28 +6,34 @@ Created on Thu Jul 13 17:52:14 2017
 @author: nilshoffmann
 """
 
-from pyomo.environ import ConcreteModel, Param, Var, Set, Constraint, Objective
+from pyomo.environ import ConcreteModel, Param, Var, Set, Constraint, Objective, RangeSet
 from pyomo.environ import minimize, NonNegativeReals
 from pyomo.opt import SolverFactory
 import pandas as pd
 import numpy as np
 
-#Time
-T = [1,2,3,4,5]
+#==============================================================================
+# --------Initialize-----------
+#==============================================================================
+data = pd.read_csv('data.csv', index_col = 0, nrows=5)
+damdata = pd.read_csv('dam.csv')
+
 model = ConcreteModel()
-model.T = Set(initialize = T)
+model.T = data.index
+model.DamRange = Set(initialize=[1,2,3,4])
 
 #==============================================================================
 # ------------Variables------------
 #==============================================================================
 #Wind
-CostWind = 1000000 #€/MW
+CostWind = 77350 #€/MW/a
 LifetimeWind = 20 #a
-FactorWind = [0.5, 0.3, 0.1, 0.6, 0.2] #Wind Factor @ hour X
+FactorWind = data['windfactor'] #Wind Factor @ hour X
 
 #PV
-CostPv = 1000000 #€/MWp
-LifetimePv= 30 #a
+<<<<<<< HEAD
+CostPv = 37120 #€/MWp/a
+LifetimePv= 25 #a
 FactorPv = [0.5, 0.3, 0.8, 0.4, 0.0] #Radiation Factor @ hour X
 
 #Water
@@ -37,45 +43,38 @@ Pwater = 0.001 #MWh/m^3
 TurbineLimit = 480 #MW
 InflowRain = 40 #m^3 @ hour X
 InflowRiver = 200 #m^3 @ hour X
+=======
+CostPv = 1000000 #€/MWp
+LifetimePv= 20 #a
+FactorPv = data['pv_factor']#Radiation Factor @ hour X
+>>>>>>> 0493c8d79f5a9bd470ae6dcb865145697a9c51f6
 
 #Demand
-DemandEnergy = [400, 500, 300, 450, 310] #MWh @ hour X
-DemandWater = [200, 300, 400, 301, 100] #m^3/h @ hour X
-
-#==============================================================================
-# ------------COST------------
-#==============================================================================
-#Costcalculation
-Cwind = CostWind #€/MW Wind, 1 year only
-Cpv = CostPv #€/MW PV, 1 year only
-Cwater = 0 #€/MWh
+DemandFactor = data['demand']
+DemandTotal = 71737.392
 
 #==============================================================================
 # ------------MODEL CONSTRUCTION------------
 #==============================================================================
+    
 #Parameter
-model.DemandWater = Set(initialize=DemandWater, ordered=True) #Water demand @ hour x
-model.DemandEnergy = Set(initialize=DemandEnergy, ordered=True) #Energy demand @ hour X
-model.FactorWind = Set(initialize=FactorWind, ordered = True) #Possible MWh by Wind @ hour X
-model.FactorPv = Set(initialize=FactorPv, ordered = True) #Possible MWh by PV @ hour X
-model.Cwind = Param(initialize=Cwind) #Price per MW Wind
-model.Cpv = Param(initialize=Cpv) #Price per MW PV
-model.Cwater = Param(initialize=Cwater) #Price of Water €/MWh
-model.TurbineLimit = Param(initialize=TurbineLimit) #Maximum Turbine Generation Capacity
-model.Pwater = Param(initialize=Pwater)
+model.DemandFactor = Set(initialize=DemandFactor, ordered=True) #Energy demand @ hour X #Possible MWh by Wind @ hour X
+model.Cwind = Param(initialize=CostWind/LifetimeWind) #Price per MW Wind
+model.Cpv = Param(initialize=CostPv/LifetimePv) #Price per MW PV
 
 #Variablen
 model.Pwind = Var(domain=NonNegativeReals) #installed MW Wind
 model.Ppv = Var(domain=NonNegativeReals) #installed MW Wind
-model.UsageWater = Var(model.T, domain=NonNegativeReals) #used M^3 Water @ hour X
+model.Dam = Var(within=model.DamRange, bounds=(1,4)) #Dam Variable to choose from
 
 #==============================================================================
 # ----------Constraint & Objective & Solver------------
 #==============================================================================
 #------Objective Function-------
-#€/MW * MW + €/MW + m^3 * MWh/m^3 * €/MWh
+        
+#Price per MW Wind * installed Wind Capacity + Price per MW Pv * installed PV capacity + Price of Dam installation
 def obj_rule(model):
-        return(model.Cwind * model.Pwind + model.Cpv * model.Ppv + sum((model.UsageWater[i] * model.Pwater * model.Cwater) for i in model.T))
+        return(model.Cwind * model.Pwind + model.Cpv * model.Ppv + damdata.loc[model.Dam,'costs'])
     
 model.cost = Objective(sense=minimize, rule=obj_rule)
 
@@ -83,25 +82,10 @@ model.cost = Objective(sense=minimize, rule=obj_rule)
 #----CONSTRAINTS-------
 #Power of Wind * WindFactor + PV * PVFactor + Waterused @ hour X * Pwater must be bigger than Energy Demand
 def DemandEnergy_rule(model, i):
-    return (model.Pwind * model.FactorWind[i] + model.Ppv * model.FactorPv[i] + model.UsageWater[i] * Pwater >= model.DemandEnergy[i])
+    return (model.Pwind * FactorWind[i] + model.Ppv * FactorPv[i] + damdata.loc[model.Dam,'power'] >= model.DemandFactor[i] * DemandTotal)
 
 model.EnergyDemand = Constraint(model.T, rule=DemandEnergy_rule)
 
-
-#Limitation through Water Turbine
-def MaxWaterPower_rule(model, i):
-    return(model.UsageWater[i] * model.Pwater  <= 480)
-
-model.MaxWaterPower = Constraint(model.T, rule=MaxWaterPower_rule)
-
-#Storage
-#for i in model.T:
-#    StorageVariable[i] = StorageVariable[i-1] - model.DemandWater[i] - model.UsageWater[i].value
-#    
-#def WaterUsage_rule (model, i):
-#    return(StorageVariable[i] >= model.DemandWater[i])
-#
-#model.WaterDemand = Constraint(model.T, rule=WaterUsage_rule)
 
 #------SOLVER---------
 opt = SolverFactory('glpk')
@@ -115,22 +99,41 @@ results = opt.solve(model, tee=True)
 print("")
 print("Wind installiert:", round(model.Pwind.value, 2), "MWh")
 print("PV installiert:", round(model.Ppv.value, 2), "MWh")
-print("Gesamt benötigtes Wasser:", round(sum(model.UsageWater[i].value for i in model.T),2), "m^3")
+print("Dam installiert:", damdata.loc[model.Dam,'power'])
 print("")
 for i in model.T:
-    print("Wasser im Speicher:", StorageVariable[i])
-    print("Nach Stunde", i)
-    print("Energy Demand:",model.DemandEnergy[i], "MWh")
-    print("Water Demand:", model.DemandWater[i], "m^3")
+    print("Stunde", i)
+    print("Energy Demand:",model.DemandFactor[i] * DemandTotal, "MWh")
     print("Zusammensetzung:", 
-          round(model.FactorPv[i]*model.Ppv.value,2), "MWh PV |", 
-          round(model.FactorWind[i]*model.Pwind.value,2), "MWh Wind |", 
-          round(model.UsageWater[i].value*Pwater,2), "MWh Wasser")
-    print(round(model.UsageWater[i].value,2), "m^3 Wasser für Energieerzeugung")
-    print("")
+          round(FactorPv[i]*model.Ppv.value,2), "MWh PV |", 
+          round(FactorWind[i]*model.Pwind.value,2), "MWh Wind |",
+          damdata.loc[model.Dam,'power'], "MWh Dam",)
 print("")
-print("Wind:", model.Cwind * model.Pwind.value, "€ |", Cwind, "€ pro MWh" )
-print("PV:", model.Cpv * model.Ppv.value, "€ |", Cpv, "€ pro MWh" )
-print("Wasser:", sum((model.UsageWater[i].value for i in model.T)) * Pwater * Cwater, "€ |", Cwater, "€ pro MWh" )
+print("Wind:", model.Cwind * model.Pwind.value, "€ |", model.Cwind.value, "€ pro MWh" )
+print("PV:", model.Cpv * model.Ppv.value, "€ |", model.Cpv.value, "€ pro MWh" )
+print("Damm:", damdata.loc[model.Dam,'costs'], "€ |", damdata.loc[model.Dam,'costs']/damdata.loc[model.Dam,'power'], "€ pro MWh" )
 print("----")
-print("Gesamtkosten:", model.Cwind * model.Pwind.value + model.Cpv * model.Ppv.value + sum((model.UsageWater[i].value for i in model.T)) * Pwater * Cwater, "€")
+print("Gesamtkosten:", model.Cwind * model.Pwind.value + model.Cpv * model.Ppv.value)
+
+'''function2(Pdam):
+    storage
+    
+for i in model.T:
+     StorageVariable[i] = StorageVariable[i-1] - WasserzuflussFunction(Pdam) + Wasserabflussfunction(Pdam)
+    
+Wasserzuflussfunction(Pdam):
+    if Pdam == 5:
+        return(Durchflussgesamt * DatenDämme.Spalte5,Pdam) #km^3
+    
+Wasserabflussfunction(Pdam):
+'''
+
+
+#Storage
+#for i in model.T:
+#   
+#    
+#def WaterUsage_rule (model, i):
+#    return(StorageVariable[i] >= model.DemandWater[i])
+#
+#model.WaterDemand = Constraint(model.T, rule=WaterUsage_rule)
